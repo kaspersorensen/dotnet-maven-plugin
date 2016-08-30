68476dc17a421ab7d86e4088fd9a1121ba38b947
kaspersorensen <i.am.kasper.sorensen@gmail.com>
2016-08-30 14:31:49 -0700

    Added example C#/dotnet project

---
+++ example-project/ModuleX/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ModuleX
+{
+    public class Program
+    {
+        public string GetMessage()
+        {
+            return "Hello world";
+        }
+
+        public static void Main(string[] args)
+        {
+            var o = new Program();
+            Console.WriteLine(o.GetMessage());
+        }
+    }
+}
