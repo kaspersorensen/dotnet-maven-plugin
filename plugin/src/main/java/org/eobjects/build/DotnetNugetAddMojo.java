78eb29219ce1a1b0d2fec0624a77730950cb9f5c
Randy Larson <rlarson@satorisoftware.com>
2017-07-10 09:21:02 -0700

    Update file formatting (via Eclipe Format Ctrl+Shift F). Rename nuget classes to represent that these functions are now driven by `dotnet`

---
+++ plugin/src/main/java/org/eobjects/build/DotnetNugetAddMojo.java
@@ -0,0 +1,47 @@
+package org.eobjects.build;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "nuget-add", defaultPhase = LifecyclePhase.INSTALL)
+public class DotnetNugetAddMojo extends AbstractDotnetMojo {
+
+	@Parameter(property = "nuget.add.source", required = false, defaultValue = "~/.nuget/packages")
+	private String nugetAddSource;
+
+	@Parameter(property = "nuget.add.enabled", required = false, defaultValue = "true")
+	private boolean nugetAddEnabled;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		if (!nugetAddEnabled) {
+			getLog().debug("Disabled, skipping");
+			return;
+		}
+
+		final PluginHelper helper = getPluginHelper();
+
+		for (File subDirectory : helper.getProjectDirectories()) {
+			try {
+				final String targetPath;
+				if (this.nugetAddSource == null || this.nugetAddSource.isEmpty()) {
+					// default
+					targetPath = System.getProperty("user.home") + "/.nuget/packages";
+				} else if (this.nugetAddSource.startsWith("~")) {
+					targetPath = System.getProperty("user.home") + this.nugetAddSource.substring(1);
+				} else {
+					targetPath = this.nugetAddSource;
+				}
+				final File nugetPackage = helper.getNugetPackage(subDirectory);
+				final String nugetPackagePath = nugetPackage.getCanonicalPath();
+				helper.executeCommand(subDirectory, "dotnet", "nuget", "push", nugetPackagePath, "-s", targetPath);
+			} catch (Exception e) {
+				throw new MojoFailureException("Command [nuget add] failed!", e);
+			}
+		}
+	}
+}
--- plugin/src/main/java/org/eobjects/build/PluginHelper.java
+++ plugin/src/main/java/org/eobjects/build/PluginHelper.java
@@ -7,171 +7,170 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 
 public final class PluginHelper {
 
-    public static final String PROPERTY_BASEDIR = "${project.basedir}";
+	public static final String PROPERTY_BASEDIR = "${project.basedir}";
 
-    public static final String PROPERTY_BUILD_DIR = "${project.build.directory}";
+	public static final String PROPERTY_BUILD_DIR = "${project.build.directory}";
 
-    public static PluginHelper get(Log log, File basedir, Map<String, String> environment, File dotnetPackOutput,
-            String buildConfiguration, boolean skip) {
-        return new PluginHelper(log, basedir, environment, dotnetPackOutput, buildConfiguration, skip);
-    }
+	public static PluginHelper get(Log log, File basedir, Map<String, String> environment, File dotnetPackOutput,
+			String buildConfiguration, boolean skip) {
+		return new PluginHelper(log, basedir, environment, dotnetPackOutput, buildConfiguration, skip);
+	}
 
-    private final File basedir;
-    private final Map<String, String> environment;
-    private final boolean skip;
-    private final File dotnetPackOutput;
-    private final String buildConfiguration;
-    private final Log log;
+	private final File basedir;
+	private final Map<String, String> environment;
+	private final boolean skip;
+	private final File dotnetPackOutput;
+	private final String buildConfiguration;
+	private final Log log;
 
-    private PluginHelper(Log log, File basedir, Map<String, String> environment, File dotnetPackOutput,
-            String buildConfiguration, boolean skip) {
-        this.log = log;
-        this.basedir = basedir;
-        this.environment = environment == null ? Collections.<String, String> emptyMap() : environment;
-        this.buildConfiguration = buildConfiguration == null ? "Release" : buildConfiguration;
-        this.dotnetPackOutput = dotnetPackOutput == null ? new File("bin") : dotnetPackOutput;
-        this.skip = skip;
-    }
+	private PluginHelper(Log log, File basedir, Map<String, String> environment, File dotnetPackOutput,
+			String buildConfiguration, boolean skip) {
+		this.log = log;
+		this.basedir = basedir;
+		this.environment = environment == null ? Collections.<String, String>emptyMap() : environment;
+		this.buildConfiguration = buildConfiguration == null ? "Release" : buildConfiguration;
+		this.dotnetPackOutput = dotnetPackOutput == null ? new File("bin") : dotnetPackOutput;
+		this.skip = skip;
+	}
+	
+	public boolean isSkip() {
+		return skip;
+	}
 
-    public boolean isSkip() {
-        return skip;
-    }
+	private final FileFilter projectFileDirectoryFilter = new FileFilter() {
+		public boolean accept(File dir) {
+			if (dir.isDirectory()) {
+				if (getProjectFile(dir, false) != null) {
+					return true;
+				}
+			}
+			return false;
+		}
+	};
 
-    private final FileFilter projectFileDirectoryFilter = new FileFilter() {
-        public boolean accept(File dir) {
-            if (dir.isDirectory()) {
-                if (getProjectFile(dir, false) != null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    };
+	private final FilenameFilter csProjFilter = new FilenameFilter() {
 
-    private final FilenameFilter csProjFilter = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			if (name.endsWith(".csproj")) {
+				return true;
+			}
+			return false;
+		}
+	};
 
-        @Override
-        public boolean accept(File dir, String name) {
-            if (name.endsWith(".csproj")) {
-                return true;
-            }
-            return false;
-        }
-    };
+	public File getNugetPackageDir(File subDirectory) {
+		if (dotnetPackOutput.isAbsolute()) {
+			return dotnetPackOutput;
+		}
+		final File directory = new File(subDirectory, dotnetPackOutput.getPath());
+		return directory;
+	}
 
-    public File getNugetPackageDir(File subDirectory) {
-        if (dotnetPackOutput.isAbsolute()) {
-            return dotnetPackOutput;
-        }
-        final File directory = new File(subDirectory, dotnetPackOutput.getPath());
-        return directory;
-    }
+	public File getNugetPackage(File subDirectory) {
+		final File packageDirectory = getNugetPackageDir(subDirectory);
+		final File[] nugetPackages = packageDirectory.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".nupkg");
+			}
+		});
 
-    public File getNugetPackage(File subDirectory) {
-        final File packageDirectory = getNugetPackageDir(subDirectory);
-        final File[] nugetPackages = packageDirectory.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".nupkg");
-            }
-        });
+		if (nugetPackages == null || nugetPackages.length == 0) {
+			throw new IllegalStateException("Could not find NuGet package! ModuleDir=" + subDirectory + ", PackageDir="
+					+ packageDirectory + ", PackOutput=" + dotnetPackOutput);
+		}
 
-        if (nugetPackages == null || nugetPackages.length == 0) {
-            throw new IllegalStateException("Could not find NuGet package! ModuleDir=" + subDirectory + ", PackageDir="
-                    + packageDirectory + ", PackOutput=" + dotnetPackOutput);
-        }
+		return nugetPackages[0];
+	}
 
-        return nugetPackages[0];
-    }
+	public File[] getProjectDirectories() throws MojoFailureException {
+		return getProjectDirectories(true);
+	}
 
-    public File[] getProjectDirectories() throws MojoFailureException {
-        return getProjectDirectories(true);
-    }
+	public File[] getProjectDirectories(boolean throwExceptionWhenNotFound) throws MojoFailureException {
+		if (skip) {
+			return new File[0];
+		}
 
-    public File[] getProjectDirectories(boolean throwExceptionWhenNotFound) throws MojoFailureException {
-        if (skip) {
-            return new File[0];
-        }
+		final File directory = basedir;
+		if (projectFileDirectoryFilter.accept(directory)) {
+			return new File[] { directory };
+		}
 
-        final File directory = basedir;
-        if (projectFileDirectoryFilter.accept(directory)) {
-            return new File[] { directory };
-        }
+		final File[] directories = directory.listFiles(projectFileDirectoryFilter);
+		if (directories == null || directories.length == 0) {
+			if (throwExceptionWhenNotFound) {
+				throw new MojoFailureException("Could not find any directories with a project/build file.");
+			} else {
+				return new File[0];
+			}
+		}
+		return directories;
+	}
 
-        final File[] directories = directory.listFiles(projectFileDirectoryFilter);
-        if (directories == null || directories.length == 0) {
-            if (throwExceptionWhenNotFound) {
-                throw new MojoFailureException("Could not find any directories with a project/build file.");
-            } else {
-                return new File[0];
-            }
-        }
-        return directories;
-    }
+	public void executeCommand(File subDirectory, String command) throws MojoFailureException {
+		executeCommand(subDirectory, command.split(" "));
+	}
 
-    public void executeCommand(File subDirectory, String command) throws MojoFailureException {
-        executeCommand(subDirectory, command.split(" "));
-    }
+	public void executeCommand(File subDirectory, String... command) throws MojoFailureException {
+		final ProcessBuilder processBuilder = new ProcessBuilder(command);
+		processBuilder.directory(subDirectory);
+		for (Entry<String, String> entry : environment.entrySet()) {
+			final String key = entry.getKey();
+			if (key != null) {
+				String value = entry.getValue();
+				if (value == null) {
+					value = "";
+				}
+				processBuilder.environment().put(key, value);
+			}
+		}
+		processBuilder.inheritIO();
 
-    public void executeCommand(File subDirectory, String... command) throws MojoFailureException {
-        final ProcessBuilder processBuilder = new ProcessBuilder(command);
-        processBuilder.directory(subDirectory);
-        for (Entry<String, String> entry : environment.entrySet()) {
-            final String key = entry.getKey();
-            if (key != null) {
-                String value = entry.getValue();
-                if (value == null) {
-                    value = "";
-                }
-                processBuilder.environment().put(key, value);
-            }
-        }
-        processBuilder.inheritIO();
+		final int exitCode;
+		try {
+			final Process process = processBuilder.start();
+			exitCode = process.waitFor();
+		} catch (Exception e) {
+			throw new MojoFailureException("Command (in " + subDirectory + ") " + Arrays.toString(command) + " failed",
+					e);
+		}
 
-        final int exitCode;
-        try {
-            final Process process = processBuilder.start();
-            exitCode = process.waitFor();
-        } catch (Exception e) {
-            throw new MojoFailureException("Command (in " + subDirectory + ") " + Arrays.toString(command) + " failed",
-                    e);
-        }
+		if (exitCode == 0) {
+			// success
+		} else {
+			throw new MojoFailureException("Command (in " + subDirectory + ") " + Arrays.toString(command)
+					+ " returned non-zero exit code: " + exitCode);
+		}
+	}
 
-        if (exitCode == 0) {
-            // success
-        } else {
-            throw new MojoFailureException("Command (in " + subDirectory + ") " + Arrays.toString(command)
-                    + " returned non-zero exit code: " + exitCode);
-        }
-    }
+	public String getBuildConfiguration() {
+		return buildConfiguration;
+	}
 
-    public String getBuildConfiguration() {
-        return buildConfiguration;
-    }
+	public DotnetProjectFile getProjectFile(File directory) {
+		return getProjectFile(directory, true);
+	}
 
-    public DotnetProjectFile getProjectFile(File directory) {
-        return getProjectFile(directory, true);
-    }
-
-    public DotnetProjectFile getProjectFile(File directory, boolean throwExceptionIfNotFound) {
-        final File projectJson = new File(directory, "project.json");
-        if (projectJson.exists()) {
-            return new ProjectJsonFile(projectJson, log);
-        }
-        final File[] csProjFiles = directory.listFiles(csProjFilter);
-        if (csProjFiles == null || csProjFiles.length == 0) {
-            if (throwExceptionIfNotFound) {
-                throw new IllegalArgumentException("Could not resolve any project/build file in directory: "
-                        + directory);
-            }
-            return null;
-        }
-        return new CsProjFile(csProjFiles[0], log);
-    }
+	public DotnetProjectFile getProjectFile(File directory, boolean throwExceptionIfNotFound) {
+		final File projectJson = new File(directory, "project.json");
+		if (projectJson.exists()) {
+			return new ProjectJsonFile(projectJson, log);
+		}
+		final File[] csProjFiles = directory.listFiles(csProjFilter);
+		if (csProjFiles == null || csProjFiles.length == 0) {
+			if (throwExceptionIfNotFound) {
+				throw new IllegalArgumentException(
+						"Could not resolve any project/build file in directory: " + directory);
+			}
+			return null;
+		}
+		return new CsProjFile(csProjFiles[0], log);
+	}
 }
